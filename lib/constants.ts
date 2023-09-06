13d42faca354e42632f9d6545012346b7d3e3e71
Maria <mariale171001@gmail.com>
2023-09-06 15:08:01 +0200

    setup: database and react-icons

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const NAVLINKS = [
     { label: "Startside", href: "/" },
-    { label: "Rum", href: "/rum" },
-    { label: "Kalender", href: "/kalender" },
-    { label: "Karakterer", href: "/karakterer" },
+    { label: "Rum", href: "/room/161" },
+    { label: "Kalender", href: "/calendar" },
+    { label: "Karakterer", href: "/grades" },
 ]
